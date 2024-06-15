32795644622c95c1109f2f95a40e5639cbdecd9e
unknown <anilkumar.banti.trainer@gmail.com>
2024-06-15 17:32:19 +0530

    Initial commit with asynchronous JavaScript examples

---
+++ CallbackDemo.js
@@ -0,0 +1,17 @@
+function fetchData(callback,timeout)
+{
+    setTimeout(() => {
+        console.log(`Data fetched`);
+        callback();
+    }, timeout);
+}
+
+console.log(`start`);
+
+//function dis() {
+   // console.log(`callback excuted`);
+//}
+//fetchData(dis,2000);
+fetchData(()=>{ console.log(`callback excuted`)},2000);
+
+
